44cbca76fa50df9e4f0e8462572abff512073162
samiassaye-21 <samuelassaye905@gmail.com>
2025-04-18 10:49:25 -0700

    bcryptjs added

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,5 @@
 const pool = require('../db');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
 const SECRET = 'your_secret_key'; // Put this in .env for production
